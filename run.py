38fae3e03037a786115c25bfb47c2dfc07c774c0
Cz0rek (laptop) <miki0123@onet.pl>
2024-06-15 16:06:57 +0200

    Naprawione cookies

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 from app import create_app
+from flask import session
 
 app = create_app()
+first_request = True
+
+@app.before_request
+def clear_sessions():
+    global first_request
+    if first_request:
+        session.clear()
+        first_request = False
 
 if __name__ == '__main__':
     app.run(debug=True)
