aa445982ed607abb85392ab0590f9d1b3caf50bf
UniSzef <hornung.bartek@gmail.com>
2024-06-15 14:32:39 +0200

    working template

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,21 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_bootstrap import Bootstrap
-from flask_dotenv import DotEnv
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 bcrypt = Bcrypt()
 bootstrap = Bootstrap()
-dotenv = DotEnv()
 
 def create_app():
     app = Flask(__name__)
-    dotenv.init_app(app)
+
+    # Load environment variables
+    load_dotenv()
+
     app.config.from_object('config.Config')
 
     db.init_app(app)
@@ -31,4 +34,5 @@ def create_app():
 
 @login_manager.user_loader
 def load_user(user_id):
+    from app.models import User
     return User.query.get(int(user_id))
