eb007f342f061379ef1326fbaafd8bee39993740
Cz0rek <miki0123@onet.pl>
2024-06-16 07:20:49 +0200

    polskie nazwy

--- app/forms.py
+++ app/forms.py
@@ -3,17 +3,17 @@ from wtforms import StringField, PasswordField, TextAreaField, SubmitField, Bool
 from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
-    email = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=1, max=128)])
-    submit = SubmitField('Log In')
+    email = StringField('Nazwa użytkownika', validators=[DataRequired()])
+    password = PasswordField('Hasło', validators=[DataRequired(), Length(min=1, max=128)])
+    submit = SubmitField('Zaloguj się')
 
 class TopicForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    body = TextAreaField('Body', validators=[DataRequired()])
-    anonymous = BooleanField('Create as Anonymous')  # Anonymity checkbox
-    submit = SubmitField('Submit')
+    title = StringField('Tytuł', validators=[DataRequired()])
+    body = TextAreaField('Treść', validators=[DataRequired()])
+    anonymous = BooleanField('Utwórz jako anonimowy')  # Anonymity checkbox
+    submit = SubmitField('Wyślij')
 
 class PostForm(FlaskForm):
-    content = TextAreaField('Content', validators=[DataRequired()])
-    anonymous = BooleanField('Create as Anonymous')  # Anonymity checkbox
-    submit = SubmitField('Add Post')
+    content = TextAreaField('Treść', validators=[DataRequired()])
+    anonymous = BooleanField('Utwórz jako anonimowy')  # Anonymity checkbox
+    submit = SubmitField('Dodaj Post')
